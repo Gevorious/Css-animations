5025adc9003aca13e55f5e6acddef4de9b155f7c
Gevorg Pahlevanyan <gevorg.pahlevanyan@gmail.com>
2020-09-22 08:48:30 +0400

    Fixed a styling issue

--- src/block/block.js
+++ src/block/block.js
@@ -5,10 +5,10 @@ const Block = ({ data, delay }) => {
     const { color, sides, pos, rot, dir, id } = data
 
     const style = {
-        borderTop: `${sides[0]}px solid transparent`,
-        borderRight: `${sides[1]}px solid transparent`,
-        borderBottom: `${sides[2]}px solid transparent`,
-        borderLeft: `${sides[3]}px solid transparent`,
+        borderWidth: `${sides[0]}px ${sides[1]}px ${sides[2]}px ${sides[3]}px`,
+        // borderRightWidth: `${sides[1]}px`,
+        // borderBottomWidth: `${sides[2]}px`,
+        // borderLeftWidth: `${sides[3]}px`,
         transform: `translate(${pos[1]}px, ${pos[0]}px) rotate(${rot}deg)`,
         transitionDelay: `${delay}s`,
     }
